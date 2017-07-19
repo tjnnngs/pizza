41e87ebc9804c6dc53c300d5e737046c456d31bf
TONYA JENNINGS <you@example.com>
2017-07-18 21:58:13 -0400

    still trying to get pizza to work...it doesn't

--- pizza.rb
+++ pizza.rb
@@ -11,26 +11,50 @@
 	end
 
 	def crust()
-		["pan", "thin crust", "thick crust"].sample
+		["pan pizza", "thin crust pizza", "thick crust pizza"].sample
 	end
 
 	def sauce()
 		["marinara sauce", "alfredo sauce", "no sauce"].sample
 	end
 
-	def toppings()
-		["extra cheese", "pepperoni", "banana peppers", "olives", "mushrooms", "onions"].sample
+	def meat_toppings()
+		["sausage", "pepperoni", "ham", "italian sausage" ].sample
 	end
 
+	def veggie_toppings()
+		["banana peppers", "green olives", "black olives", "mushrooms", "onions", "green peppers", "pineapples", "tomatoes", "anchovies"].sample
+	end
+
+	def special_requests()
+		["no special requests", "extra pepperoni", "extra cheese"].sample 
+		
+	end
+
+		
 	def pizza_maker()
 		take_order.times do 
-			p "You ordered 1 #{size}, #{crust}, #{sauce}, #{toppings} and #{toppings} pizza"
+			p "You ordered 1 #{size}, #{crust}, with #{sauce}, #{meat_toppings}, #{veggie_toppings} with #{special_requests}"
 
-		end
-			
 	end
 
+		def cost.keys
+		cost => [personal, small, medium, large]
+end
+
+		def cost.values
+		=>[5, 7,10,12]
+	end
+
+end
+
+		p "Your total is $ cost.value{personal} + cost.value{small} + cost.value{medium} + cost.value{large}"
+
+
 pizza_maker
+
+
+
 
 
 	
